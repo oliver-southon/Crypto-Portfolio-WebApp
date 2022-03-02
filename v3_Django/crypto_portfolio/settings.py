55a68f15dc18d1ad029fb0d98ecf910c5a836ab3
Oliver <oliversouthon@gmail.com>
2022-03-02 15:48:02 +1100

    django-implementation

--- v3_Django/crypto_portfolio/settings.py
+++ v3_Django/crypto_portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from secret_key import key 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = key
+SECRET_KEY = os.environ.get("CRYPTOTRACKER_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

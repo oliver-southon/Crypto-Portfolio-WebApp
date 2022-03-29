5015189f6a892d244c6a9fb63685fb15ff406697
oliver-southon <oliversouthon@gmail.com>
2022-03-29 22:50:21 +1100

    python-anywhere-ready

--- v2_webApp/model.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm 
-from wtforms import SubmitField, StringField, DecimalField, validators
-
-class HoldingForm(FlaskForm): # inherits 'Form' class from flask_wtf
-    symbol = StringField('symbol', [validators.DataRequired()])
-    entry_price = DecimalField('entry_price', [validators.DataRequired()])
-    entry_amt = DecimalField('amt', [validators.DataRequired()])
-    
-    submit = SubmitField('Submit')
